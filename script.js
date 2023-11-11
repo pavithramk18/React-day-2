aa3bf910503f83fafce67dfadeaba62c39d140ce
pavithramk18 <pavithram.kumaresan@gmail.com>
2023-11-11 18:07:48 +0530

    save

---
+++ script.js
@@ -0,0 +1,3 @@
+//npm init-It should be give only on the empty folder and it is used o initialize the react project and mainly used to extract the npm packages as a JSON 
+//npm install bootstrap-To include bootstrap into react app
+//npmjs.com-List of npm packages already created
